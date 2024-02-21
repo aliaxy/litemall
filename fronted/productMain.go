ccac40810df4375a57e567fc5c0cd474e73023f2
aliaxy <aruvelut00@163.com>
2024-02-21 17:47:06 +0800

    添加部署代码

---
+++ fronted/productMain.go
@@ -0,0 +1,21 @@
+// Package main
+package main
+
+// import (
+// 	"github.com/kataras/iris/v12"
+// )
+
+// func main() {
+// 	// 创建 iris 实例
+// 	app := iris.New()
+
+// 	// 设置模版目标
+// 	app.HandleDir("/public", "./fronted/web/public")
+// 	app.HandleDir("/html", "./fronted/web/htmlProductShow")
+
+// 	app.Run(
+// 		iris.Addr("localhost:8083"),
+// 		iris.WithoutServerError(iris.ErrServerClosed),
+// 		iris.WithOptimizations,
+// 	)
+// }
