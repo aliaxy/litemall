61c807ed735107eaa393f8d935c42b5f4e2d6546
aliaxy <aruvelut00@163.com>
2024-02-21 17:35:46 +0800

    生成商品详情静态文件

--- fronted/web/controller/product.go
+++ fronted/web/controller/product.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"html/template"
+	"os"
+	"path/filepath"
 	"strconv"
 
 	"litemall/model"
@@ -17,6 +20,63 @@ type ProductController struct {
 	ProductService service.IProductService
 	OrderService   service.IOrderService
 	Session        *sessions.Session
+}
+
+var (
+	htmlOutPath  = "./fronted/web/htmlProductShow/" // 生成的 html 保存目录
+	templatePath = "./fronted/web/view/template/"   // 静态文件模版目录
+)
+
+// GetGenerateHtml 生成文件
+func (p *ProductController) GetGenerateHtml() {
+	// 获取模版文件地址
+	contentTmpl, err := template.ParseFiles(filepath.Join(templatePath, "product.html"))
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	productID, err := p.Ctx.URLParamInt64("productID")
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	// 获取 html 生成路径
+	fileName := filepath.Join(htmlOutPath, "htmlProduct.html")
+
+	// 获取模板渲染数据
+	product, err := p.ProductService.GetProductByID(productID)
+	if err != nil {
+		p.Ctx.Application().Logger().Debug(err)
+	}
+
+	// 生成静态文件
+	generateStaticHTML(p.Ctx, contentTmpl, fileName, product)
+}
+
+// generateStaticHTML 生成静态文件
+func generateStaticHTML(ctx iris.Context, template *template.Template, fileName string, product *model.Product) {
+	// 判断静态文件是否存在
+	if fileExist(fileName) {
+		err := os.Remove(fileName)
+		if err != nil {
+			ctx.Application().Logger().Debug(err)
+		}
+	}
+
+	// 生成静态文件
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		ctx.Application().Logger().Debug(err)
+	}
+	defer file.Close()
+
+	template.Execute(file, product)
+}
+
+// fileExist 判断文件是否存在
+func fileExist(fileName string) bool {
+	_, err := os.Stat(fileName)
+	return err == nil || os.IsExist(err)
 }
 
 // GetDetail 商品详情页面
